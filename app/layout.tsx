33a793ada760d349c4a01e222b039f96738df08e
Eric Lee <lethaikhoa0109@gmail.com>
2025-05-09 16:50:48 +0700

    Fix first deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,71 +1,12 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import Navbar from "@/app/components/Navbar";
 
-const workSans = localFont({
-  src: [
-    {
-      path: "./fonts/WorkSans-Black.ttf",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-ExtraBold.ttf",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Thin.ttf",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Light.ttf",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-ExtraLight.ttf",
-      weight: "100",
-      style: "normal",
-    },
-  ],
-  variable: "--font-work-sans",
-});
-
-export const metadata: Metadata = {
-  title: "YC Directory",
-  description: "Pitch, click and grow your startup",
-};
-
-export default function RootLayout({
+export default function Layout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body className={workSans.variable}>{children}</body>
-    </html>
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+    </main>
   );
 }
