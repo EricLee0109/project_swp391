0440160caaf427d6300c3106959d3cc9f382b2a7
AnhPhi2002 <phinhase161377@fpt.edu.vn>
2025-06-12 22:26:53 +0700

    fix auth.ts

--- auth.ts
+++ auth.ts
@@ -2,15 +2,40 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const authOptions = {
   providers: [
     GitHub({
-      clientId: process.env.AUTH_WEBAPP_GITHUB_CLIENT_ID,
-      clientSecret: process.env.AUTH_WEBAPP_GITHUB_CLIENT_SECRET,
+      clientId: process.env.AUTH_WEBAPP_GITHUB_CLIENT_ID!,
+      clientSecret: process.env.AUTH_WEBAPP_GITHUB_CLIENT_SECRET!,
     }),
     Google({
-      clientId: process.env.AUTH_WEBAPP_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.AUTH_WEBAPP_GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_WEBAPP_GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.AUTH_WEBAPP_GOOGLE_CLIENT_SECRET!,
     }),
   ],
-});
+  secret: process.env.AUTH_SECRET,
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth(authOptions);
+
+// import NextAuth from "next-auth";
+// import GitHub from "next-auth/providers/github";
+// import Google from "next-auth/providers/google";
+
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [
+//     GitHub({
+//       clientId: process.env.AUTH_WEBAPP_GITHUB_CLIENT_ID,
+//       clientSecret: process.env.AUTH_WEBAPP_GITHUB_CLIENT_SECRET,
+//     }),
+//     Google({
+//       clientId: process.env.AUTH_WEBAPP_GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.AUTH_WEBAPP_GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+// });
+
+// lib/auth.ts
+
+
+
