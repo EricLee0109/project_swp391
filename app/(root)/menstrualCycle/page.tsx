cb5053db913f0cf866abb0582970d13f0901e6b5
Eric Lee <lethaikhoa0109@gmail.com>
2025-06-18 17:26:03 +0700

    Selection options, Menstrual Cycle

--- app/(root)/menstrualCycle/page.tsx
+++ app/(root)/menstrualCycle/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import React, { useState } from "react";
 import MenstrualCycleTracker from "@/components/MenstrualCycle";
+import MenstrualCycleSetup from "@/components/MenstrualCycleSetup";
 
 export default function MenstrualCyclePage() {
-  return <MenstrualCycleTracker />;
+  const [isSetupOpen, setIsSetupOpen] = useState(true);
+
+  // Function to close the modal
+  const handleCloseSetup = () => {
+    setIsSetupOpen(false);
+  };
+
+  return (
+    <>
+      <div className="section">
+        {/* The tracker will be visible in the background */}
+        <MenstrualCycleTracker />
+      </div>
+
+      {/* Conditionally render the setup modal */}
+      {isSetupOpen && <MenstrualCycleSetup onClose={handleCloseSetup} />}
+    </>
+  );
 }
