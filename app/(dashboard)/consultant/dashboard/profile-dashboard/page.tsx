bbe5f31ae61d9a1793c0bc8d3be6dc7225d22228
Eric Lee <lethaikhoa0109@gmail.com>
2025-07-03 17:12:07 +0700

    Deploy again 2

--- app/(dashboard)/consultant/dashboard/profile-dashboard/page.tsx
+++ app/(dashboard)/consultant/dashboard/profile-dashboard/page.tsx
@@ -1,6 +1,6 @@
 // app/(consultant)/dashboard/profile/page.tsx
 
-import Header from "@/components/dashboard/header";
+// import Header from "@/components/dashboard/header";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import Link from "next/link";
@@ -12,17 +12,15 @@ export default async function DashboardPage() {
   const session = await auth();
   const user = session?.user;
 
-
   const isConsultant = user?.role === "Consultant";
-
 
   return (
     <div>
-      <Header
+      {/* <Header
         title="Quản lý hồ sơ"
         href="/dashboard/profile"
         currentPage="Hồ sơ tư vấn viên"
-      />
+      /> */}
 
       <div className="container mx-auto p-6 space-y-6">
         {isConsultant && (
