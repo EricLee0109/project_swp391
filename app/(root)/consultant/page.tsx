2e20dcd27e048330ae36cd3dc4d80e2e74bc7c48
Eric Lee <lethaikhoa0109@gmail.com>
2025-07-03 19:27:59 +0700

    Deploy again 6

--- app/(root)/consultant/page.tsx
+++ app/(root)/consultant/page.tsx
@@ -1,18 +1,20 @@
 import Consultant from "@/components/consultant-page/Consultant";
 import MaxWidthWrapper from "@/components/profile/MaxWidthWrapper";
 import { ConsultantProfile } from "@/types/user/User";
+import { getAllConsultantProfiles } from "@/app/api/consultant/action";
 
-async function fetchAllConsultant() {
-  const response = await fetch(
-    `${process.env.BE_BASE_URL}/auth/profile/consultants/all`,
-    { method: "GET", next: { revalidate: 3000 } }
-  );
-  const data: ConsultantProfile[] = await response.json();
-  return data;
-}
+// async function fetchAllConsultant() {
+//   const response = await fetch(
+//     `${process.env.BE_BASE_URL}/auth/profile/consultants/all`,
+//     { method: "GET", next: { revalidate: 3000 } }
+//   );
+//   const data: ConsultantProfile[] = await response.json();
+//   return data;
+// }
 
 export default async function ConsultantsPage() {
-  const consultants = await fetchAllConsultant();
+  const consultants: ConsultantProfile[] | null =
+    await getAllConsultantProfiles();
 
   console.log(consultants, "consultants");
   return (
