faaf8ed59ac44e5749a627676ef1533efdf09dff
AnhPhi2002 <phinhase161377@fpt.edu.vn>
2025-07-29 21:11:52 +0700

    profile file nav

---
+++ app/(root)/profile/ComingSoon/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ComingSoon from "@/components/share/ComingSoon";
+
+export default function ProfilePage() {
+  return (
+  <div className="pb-20">
+    <ComingSoon />
+  </div>
+  ) 
+}
